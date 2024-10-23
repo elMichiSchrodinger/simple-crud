3a19b8e24776687e98e1647a32574ef2d47f9476
elMichiSchrodinger <darcklight47@gmail.com>
2024-10-23 17:20:31 -0500

    Crud-Simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Empleado } from './models/empleado';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'simple-crud';
+  empleadoArreglo: Empleado[]=[
+    {id:1, nombre:"Ryan",pais:"USA"},
+    {id:2, nombre:"Angelica",pais:"USA"},
+    {id:3, nombre:"Joe",pais:"USA"}
+  ]
+
+  selectedEmployee: Empleado= new Empleado();
+
+  AgregaroEditar(){
+    if (this.selectedEmployee.id == 0) {
+      this.selectedEmployee.id= this.empleadoArreglo.length+1;
+      this.empleadoArreglo.push(this.selectedEmployee);
+    }
+    this.selectedEmployee= new Empleado();
+  }
+  
+  openForEdit(empleado:Empleado){
+    this.selectedEmployee= empleado;
+  }
+
+  eliminar(){
+    if(confirm('Estas seguro de eliminar')){
+      this.empleadoArreglo = this.empleadoArreglo.filter(elemento => elemento!=this.selectedEmployee);
+      this.selectedEmployee= new Empleado();
+    }
+  }
 }
